b60d3b8bde800932636f94ac21eac10c60f39010
lesta-2 <39772702+lesta-2@users.noreply.github.com>
2022-11-04 01:25:13 +0700

    Add files via upload

---
+++ add/certv2ray.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+clear
+echo start
+sleep 0.5
+source /var/lib/crot/ipvps.conf
+domain=$(cat /etc/xray/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+mkdir /root/.acme.sh
+curl https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --set-default-ca  --server  letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
+bash acme.sh --register-account -m kimochilol@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+
